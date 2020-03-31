488614e650bb472aaac9627017a45d3949ef71de
Andreus Silva Faria <faria.andreus@gmail.com>
2020-03-31 00:28:07 -0400

    Initial commit

---
+++ PrimalLauncherDataViewer/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PrimalLauncherDataViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tcMainGrid = new System.Windows.Forms.TabControl();
+            this.lbSheets = new System.Windows.Forms.ListBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1424, 805);
+            this.panel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.88764F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.11236F));
+            this.tableLayoutPanel1.Controls.Add(this.tcMainGrid, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbSheets, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.093168F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.90683F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1424, 805);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tcMainGrid
+            // 
+            this.tcMainGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMainGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tcMainGrid.Location = new System.Drawing.Point(214, 44);
+            this.tcMainGrid.Name = "tcMainGrid";
+            this.tcMainGrid.SelectedIndex = 0;
+            this.tcMainGrid.Size = new System.Drawing.Size(1207, 758);
+            this.tcMainGrid.TabIndex = 13;
+            // 
+            // lbSheets
+            // 
+            this.lbSheets.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbSheets.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSheets.FormattingEnabled = true;
+            this.lbSheets.ItemHeight = 16;
+            this.lbSheets.Location = new System.Drawing.Point(3, 44);
+            this.lbSheets.Name = "lbSheets";
+            this.lbSheets.Size = new System.Drawing.Size(205, 758);
+            this.lbSheets.TabIndex = 7;
+            this.lbSheets.SelectedIndexChanged += new System.EventHandler(this.lbSheets_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.txtSearch);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(214, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1207, 35);
+            this.panel3.TabIndex = 10;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(1123, 6);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Go";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(802, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search:";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(849, 8);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(271, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 805);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox lbSheets;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TabControl tcMainGrid;
+    }
+}
+
---
+++ PrimalLauncherDataViewer/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PrimalLauncherDataViewer
+{
+    public partial class Form1 : Form
+    {
+        GameDataFile GameDataFile { get; } = new GameDataFile();
+        
+
+        public Form1()
+        {
+            InitializeComponent();;           
+
+            foreach (var item in GameDataFile.Index.OrderBy(x=>x.Key))
+                lbSheets.Items.Add(item.Key);
+
+            
+        }
+
+        private void lbSheets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected = lbSheets.SelectedItem.ToString();
+
+            if (tcMainGrid.TabPages.ContainsKey(selected))
+            {
+                tcMainGrid.SelectedTab = tcMainGrid.TabPages[selected];
+            }
+            else
+            {
+                DataTable dt = GameDataFile.GetGameData(selected);
+
+                DataGridView dg = new DataGridView
+                {
+                    DataSource = dt,
+                    ReadOnly = false,
+                    Dock = DockStyle.Fill
+                };
+
+                // Double buffering can make DGV slow in remote desktop
+                if (!SystemInformation.TerminalServerSession)
+                {
+                    Type dgvType = dg.GetType();
+                    PropertyInfo pi = dgvType.GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+                    pi.SetValue(dg, true, null);
+                }
+
+                TabPage newTab = new TabPage
+                {
+                    Name = selected.Replace("/", ""),
+                    Text = selected,
+                    Dock = DockStyle.Fill
+                };
+                newTab.Controls.Add(dg);
+
+                tcMainGrid.TabPages.Add(newTab);
+                tcMainGrid.SelectedTab = newTab;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if(tcMainGrid.TabPages.Count > 0)
+            {
+                DataGridView dataGrid =  (DataGridView)tcMainGrid.SelectedTab.Controls[0];
+                int firstSelected = -1;
+                string searchValue = txtSearch.Text.ToLower();
+                dataGrid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                try
+                {
+                    foreach (DataGridViewRow row in dataGrid.Rows)
+                    {
+                        for (int i = 0; i < row.Cells.Count; i++)
+                        {
+
+                            int rowIndex = row.Index;
+                            if (row.Cells[i].Value != null && row.Cells[i].Value.ToString().ToLower().Contains(searchValue))
+                            {
+                                if (firstSelected < 0)
+                                    firstSelected = row.Index;
+
+                               
+                                dataGrid.Rows[rowIndex].Selected = true;
+                                break;
+                            }
+                            else
+                            {
+                                //dataGrid.Rows[rowIndex].Visible = false;
+                            }
+                        }
+                    }
+
+                    if (firstSelected < 0)
+                        MessageBox.Show("'" + searchValue + "' not found in this table.", "Not found");
+                    else
+                        dataGrid.FirstDisplayedScrollingRowIndex = firstSelected;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No data section opened.", "Error");
+            }
+           
+
+            
+        }
+    }
+}
